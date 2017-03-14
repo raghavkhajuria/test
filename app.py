b9c5b72457c0526f98235f26b7e576f96eff478a
raghav <rk@quippp.com>
2017-03-14 21:06:25 +0530

    some changes

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import json
+import os
 
 from flask import Flask, request, abort, jsonify, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index_new():
     return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
